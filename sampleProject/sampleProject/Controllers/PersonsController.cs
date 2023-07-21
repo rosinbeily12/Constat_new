9560fd6949eb4d42a70587615eabcbc8ec1a28d2
rosinbeily12 <98732884+rosinbeily12@users.noreply.github.com>
2023-07-21 10:21:07 +0530

    Add files via upload

---
+++ sampleProject/sampleProject/Controllers/PersonsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampleProject.Controllers
+{
+    public class PersonsController : Controller
+    {       
+        [Route("person/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
